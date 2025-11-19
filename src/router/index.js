9a2957eaf34e8e9e502383e8acb98597b7238c04
Menmery <2053186460@qq.com>
2025-11-19 14:39:59 +0800

    子路由基础配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import Layout from '@/pages/Layout/index'
 import Login from '@/pages/Login/index'
+import Home from '@/pages/Home/index'
+import Article from '@/pages/Article/index'
+import Publish from '@/pages/Publish/index'
 
 import { createBrowserRouter } from 'react-router-dom'
 import { AuthRoute } from '@/components/AuthRoute'
@@ -7,7 +10,21 @@ import { AuthRoute } from '@/components/AuthRoute'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <AuthRoute><Layout /></AuthRoute>
+    element: <AuthRoute><Layout /></AuthRoute>,
+    children: [
+      {
+        path: 'home',
+        element: <Home />
+      },
+      {
+        path: 'article',
+        element: <Article />
+      },
+      {
+        path: 'publish',
+        element: <Publish />
+      }
+    ]
   },
   {
     path: '/login',
